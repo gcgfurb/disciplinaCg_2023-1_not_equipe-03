c15e3ec0a71c912bd48a8e6fa53e201a8a5f456c
EricoHK <ericohq12@gmail.com>
2023-05-07 18:54:45 -0300

    Atividade 5 - Teste do círculo maior incompleto

---
+++ Unidade2/Atividade2/CG_N2_5/Circulo.cs
@@ -0,0 +1,47 @@
+using CG_Biblioteca;
+using OpenTK.Graphics.OpenGL4;
+
+namespace gcgcg
+{
+  internal class Circulo : Objeto
+  {
+    public double raio = 0;
+    public double centroX = 0;
+    public double centroY = 0;
+
+    public Circulo(Objeto paiRef, double raio, double centroX, double centroY) : base(paiRef)
+    {
+        PrimitivaTipo = PrimitiveType.LineLoop;
+        PrimitivaTamanho = 1;
+        this.raio = raio;
+        this.centroX = centroX;
+        this.centroY = centroY;
+
+        desenhaCirc(raio, centroX, centroY);
+    }
+
+    public void desenhaCirc() {
+        base.pontosLista.Clear();
+        for (int i = 0; i < 360; i += 5) {
+            Ponto4D ponto = new Ponto4D(Matematica.GerarPtosCirculo(i, raio));
+            ponto.X += centroX;
+            ponto.Y += centroY;
+
+            PrimitivaTamanho = 5;
+            base.PontosAdicionar(ponto);
+      }
+    }
+
+    public void desenhaCirc(double raio, double centroX, double centro) {
+        base.pontosLista.Clear();
+        for (int i = 0; i < 360; i += 5) {
+            Ponto4D ponto = new Ponto4D(Matematica.GerarPtosCirculo(i, raio));
+            ponto.X += centroX;
+            ponto.Y += centroY;
+
+            PrimitivaTamanho = 5;
+            base.PontosAdicionar(ponto);
+      }
+    }
+  }
+}
---
+++ Unidade2/Atividade2/CG_N2_5/Mundo.cs
@@ -0,0 +1,366 @@
+﻿#define CG_Gizmo  // debugar gráfico.
+#define CG_OpenGL // render OpenGL.
+//#define CG_DirectX // render DirectX.
+// #define CG_Privado // código do professor.
+
+using CG_Biblioteca;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using OpenTK.Mathematics;
+
+//FIXME: padrão Singleton
+
+namespace gcgcg
+{
+  public class Mundo : GameWindow
+  {
+    private List<Objeto> objetosLista = new List<Objeto>();
+    private Objeto objetoSelecionado = null;
+    private char rotulo = '@';
+
+    private readonly float[] _sruEixos =
+    {
+      -0.5f,  0.0f,  0.0f, /* X- */      0.5f,  0.0f,  0.0f, /* X+ */
+       0.0f, -0.5f,  0.0f, /* Y- */      0.0f,  0.5f,  0.0f, /* Y+ */
+       0.0f,  0.0f, -0.5f, /* Z- */      0.0f,  0.0f,  0.5f, /* Z+ */
+    };
+
+    private int _vertexBufferObject_sruEixos;
+    private int _vertexArrayObject_sruEixos;
+
+    private Shader _shaderVermelha;
+    private Shader _shaderVerde;
+    private Shader _shaderAzul;
+
+    private bool _firstMove = true;
+    private Vector2 _lastPos;
+    private List<Objeto> CircMenor = new List<Objeto>();
+    private List<Objeto> CircMaior = new List<Objeto>();
+    private List<Objeto> BBox = new List<Objeto>();
+    private List<Objeto> retasBBox = new List<Objeto>();
+
+    public Mundo(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+           : base(gameWindowSettings, nativeWindowSettings)
+    {
+    }
+
+    private void ObjetoNovo(Objeto objeto, Objeto objetoFilho = null)
+    {
+      if (objetoFilho == null)
+      {
+        objetosLista.Add(objeto);
+        objeto.Rotulo = rotulo = Utilitario.charProximo(rotulo);
+        objeto.ObjetoAtualizar();
+        objetoSelecionado = objeto;
+      }
+      else
+      {
+        objeto.FilhoAdicionar(objetoFilho);
+        objetoFilho.Rotulo = rotulo = Utilitario.charProximo(rotulo);
+        objetoFilho.ObjetoAtualizar();
+        objetoSelecionado = objetoFilho;
+      }
+    }
+
+    private void criaBBox() {
+      // Criando a BBox.
+      Ponto4D pontoRetangulo1 = Matematica.GerarPtosCirculo(225, 0.3);
+      pontoRetangulo1.X += 0.3;
+      pontoRetangulo1.Y += 0.3;
+      Ponto4D pontoRetangulo2 = Matematica.GerarPtosCirculo(45, 0.3);
+      pontoRetangulo2.X += 0.3;
+      pontoRetangulo2.Y += 0.3;
+      Retangulo r = new Retangulo(null, pontoRetangulo1, pontoRetangulo2);
+      BBox.Add(r);
+      ObjetoNovo(r);
+
+      SegReta ret = null;
+      for (int i = 0; i < 3; i++) {
+          ret = new SegReta(null, r.PontosId(i), r.PontosId(i+1));
+          retasBBox.Add(ret);
+          ObjetoNovo(ret);
+      }
+
+      ret = new SegReta(null, r.PontosId(0), r.PontosId(3));
+      retasBBox.Add(ret);
+      ObjetoNovo(ret);
+    }
+
+    protected override void OnLoad()
+    {
+      base.OnLoad();
+
+      GL.ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+      // Eixos
+      _vertexBufferObject_sruEixos = GL.GenBuffer();
+      GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject_sruEixos);
+      GL.BufferData(BufferTarget.ArrayBuffer, _sruEixos.Length * sizeof(float), _sruEixos, BufferUsageHint.StaticDraw);
+      _vertexArrayObject_sruEixos = GL.GenVertexArray();
+      GL.BindVertexArray(_vertexArrayObject_sruEixos);
+      GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+      GL.EnableVertexAttribArray(0);
+      _shaderVermelha = new Shader("Shaders/shader.vert", "Shaders/shaderVermelha.frag");
+      _shaderVerde = new Shader("Shaders/shader.vert", "Shaders/shaderVerde.frag");
+      _shaderAzul = new Shader("Shaders/shader.vert", "Shaders/shaderAzul.frag");
+
+      // Objeto objetoNovo = null;
+
+      #region BBox
+      // Criando o círculo menor.
+      Circulo c_menor = new Circulo(null, 0.1, 0.3, 0.3);
+      ObjetoNovo(c_menor);
+
+      Ponto p_c_menor = new Ponto(null, new Ponto4D(0.3, 0.3));
+      p_c_menor.PrimitivaTamanho = 10;
+      ObjetoNovo(p_c_menor);
+
+      // Guardando objetos para depois.
+      CircMenor.Add(c_menor);
+      CircMenor.Add(p_c_menor);
+
+      // Criando o círculo maior.
+      Circulo c_maior = new Circulo(null, 0.3, 0.3, 0.3);
+      ObjetoNovo(c_maior);
+      CircMaior.Add(c_maior);
+
+      criaBBox();
+      #endregion
+
+      /*
+      #region Objeto: polígono qualquer  
+      objetoNovo = new Poligono(null);
+      objetoNovo.PontosAdicionar(new Ponto4D(0.25, 0.25));
+      objetoNovo.PontosAdicionar(new Ponto4D(0.75, 0.25));
+      objetoNovo.PontosAdicionar(new Ponto4D(0.75, 0.75));
+      objetoNovo.PontosAdicionar(new Ponto4D(0.50, 0.50));
+      objetoNovo.PontosAdicionar(new Ponto4D(0.25, 0.75));
+      ObjetoNovo(objetoNovo); objetoNovo = null;
+      #endregion
+      #region NÃO USAR: declara um objeto filho ao polígono
+      objetoNovo = new Ponto(null, new Ponto4D(0.50, 0.75));
+      ObjetoNovo(objetosLista[0], objetoNovo); objetoNovo = null;
+      #endregion
+
+      #region Objeto: retângulo  
+      objetoNovo = new Retangulo(null, new Ponto4D(-0.25, 0.25), new Ponto4D(-0.75, 0.75));
+      objetoNovo.PrimitivaTipo = PrimitiveType.LineLoop;
+      ObjetoNovo(objetoNovo); objetoNovo = null;
+      #endregion
+
+      #region Objeto: segmento de reta  
+      objetoNovo = new SegReta(null, new Ponto4D(-0.25, -0.25), new Ponto4D(-0.75, -0.75));
+      ObjetoNovo(objetoNovo); objetoNovo = null;
+      #endregion
+
+      #region Objeto: ponto  
+      objetoNovo = new Ponto(null, new Ponto4D(0.25, -0.25));
+      objetoNovo.PrimitivaTipo = PrimitiveType.Points;
+      objetoNovo.PrimitivaTamanho = 10;
+      ObjetoNovo(objetoNovo); objetoNovo = null;
+      #endregion
+      */
+
+#if CG_Privado
+      #region Objeto: circulo  
+      objetoNovo = new Circulo(null, 0.2, new Ponto4D());
+      objetoNovo.shaderCor = new Shader("Shaders/shader.vert", "Shaders/shaderAmarela.frag");
+      ObjetoNovo(objetoNovo); objetoNovo = null;
+      #endregion
+
+      #region Objeto: SrPalito  
+      objetoNovo = new SrPalito(null);
+      ObjetoNovo(objetoNovo); objetoNovo = null;
+      SrPalito objSrPalito = objetoSelecionado as SrPalito;
+      #endregion
+
+      #region Objeto: Spline
+      objetoNovo = new Spline(null);
+      ObjetoNovo(objetoNovo); objetoNovo = null;
+      Spline objSpline = objetoSelecionado as Spline;
+      #endregion
+#endif
+
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+      base.OnRenderFrame(e);
+
+      GL.Clear(ClearBufferMask.ColorBufferBit);
+
+#if CG_Gizmo      
+      Sru3D();
+#endif
+      for (var i = 0; i < objetosLista.Count; i++)
+        objetosLista[i].Desenhar();
+
+      SwapBuffers();
+    }
+
+    private bool testeCircMaior() {
+        Circulo c = (Circulo) CircMaior[0];
+        Ponto p = (Ponto) CircMenor[1];
+
+        double dist = 0;
+        for (int i = 0; i < 72; i++) {
+            dist = Math.Pow(c.PontosId(i).X - p.PontosId(0).X, 2);
+            Console.WriteLine("Só X: " + dist);
+            dist += Math.Pow(c.PontosId(i).Y - p.PontosId(0).Y, 2);
+            Console.WriteLine(dist);
+            if (dist == 0)
+                return true;
+
+                // TODO: Ainda não tá funcionando.
+        }
+
+        return false;
+    }
+
+    private void testeBBox() {
+        if (BBox.Count == 0)
+            // Se BBox foi destruída, remonta para fazer o teste.
+            criaBBox();
+
+        Ponto p = (Ponto) CircMenor[1];
+        Retangulo r = (Retangulo) BBox[0];
+
+        if ((p.PontosId(0).X < r.PontosId(0).X) ||
+            (p.PontosId(0).Y < r.PontosId(0).Y) ||
+            (p.PontosId(0).X > r.PontosId(2).X) ||
+            (p.PontosId(0).Y > r.PontosId(2).Y)) {
+            // Extrapolou limites da BBox
+            foreach (Objeto b in retasBBox) {
+                b.PontosAlterar(new Ponto4D(0.3, 0.3), 0);
+                b.PontosAlterar(new Ponto4D(0.3, 0.3), 1);
+                b.ObjetoAtualizar();
+            }
+
+            BBox.Clear();
+        }
+    }
+
+    private void atualizaCircMenor(char direcao) {
+        Circulo c = (Circulo) CircMenor[0];
+        Ponto p = (Ponto) CircMenor[1];
+
+        if (testeCircMaior())
+            return;
+
+        switch(direcao) {
+            case 'C':
+                c.centroY += 0.01;
+                c.desenhaCirc();
+                c.ObjetoAtualizar();
+
+                // Atualiza Ponto central.
+                p.PontosId(0).Y += 0.01;
+                p.ObjetoAtualizar();
+                break;
+            case 'B':
+                c.centroY -= 0.01;
+                c.desenhaCirc();
+                c.ObjetoAtualizar();
+
+                // Atualiza Ponto central.
+                p.PontosId(0).Y -= 0.01;
+                p.ObjetoAtualizar();
+                break;
+            case 'E':
+                c.centroX -= 0.01;
+                c.desenhaCirc();
+                c.ObjetoAtualizar();
+
+                // Atualiza Ponto central.
+                p.PontosId(0).X -= 0.01;
+                p.ObjetoAtualizar();
+                break;
+            case 'D':
+                c = (Circulo) CircMenor[0];
+                c.centroX += 0.01;
+                c.desenhaCirc();
+                c.ObjetoAtualizar();
+
+                // Atualiza Ponto central.
+                p.PontosId(0).X += 0.01;
+                p.ObjetoAtualizar();
+                break;
+        }
+
+        testeBBox();
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+      base.OnUpdateFrame(e);
+
+      #region Teclado
+      var input = KeyboardState;
+      if (input.IsKeyPressed(Keys.C)) {
+          atualizaCircMenor('C');
+      } else if (input.IsKeyPressed(Keys.B)) {
+          atualizaCircMenor('B');
+      } else if (input.IsKeyPressed(Keys.E)) {
+          atualizaCircMenor('E');
+      } else if (input.IsKeyPressed(Keys.D)) {
+          atualizaCircMenor('D');
+      }      
+      #endregion
+    }
+
+    protected override void OnResize(ResizeEventArgs e)
+    {
+      base.OnResize(e);
+
+      GL.Viewport(0, 0, Size.X, Size.Y);
+    }
+
+    protected override void OnUnload()
+    {
+      foreach (var objeto in objetosLista)
+      {
+        objeto.OnUnload();
+      }
+
+      GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+      GL.BindVertexArray(0);
+      GL.UseProgram(0);
+
+      GL.DeleteBuffer(_vertexBufferObject_sruEixos);
+      GL.DeleteVertexArray(_vertexArrayObject_sruEixos);
+
+      GL.DeleteProgram(_shaderVermelha.Handle);
+      GL.DeleteProgram(_shaderVerde.Handle);
+      GL.DeleteProgram(_shaderAzul.Handle);
+
+      base.OnUnload();
+    }
+
+#if CG_Gizmo
+    private void Sru3D()
+    {
+#if CG_OpenGL && !CG_DirectX
+      GL.BindVertexArray(_vertexArrayObject_sruEixos);
+      // EixoX
+      _shaderVermelha.Use();
+      GL.DrawArrays(PrimitiveType.Lines, 0, 2);
+      // EixoY
+      _shaderVerde.Use();
+      GL.DrawArrays(PrimitiveType.Lines, 2, 2);
+      // EixoZ
+      _shaderAzul.Use();
+      GL.DrawArrays(PrimitiveType.Lines, 4, 2);
+#elif CG_DirectX && !CG_OpenGL
+      Console.WriteLine(" .. Coloque aqui o seu código em DirectX");
+#elif (CG_DirectX && CG_OpenGL) || (!CG_DirectX && !CG_OpenGL)
+      Console.WriteLine(" .. ERRO de Render - escolha OpenGL ou DirectX !!");
+#endif
+    }
+#endif    
+
+  }
+}
